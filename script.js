165ecdeafc266f7b2c1d46a3ce402c2260c15c58
AppleofGod1 <aogfoundation2025@gmail.com>
2025-06-11 21:38:35 +0000

    Update HTML, CSS, and JS files; add new images and essentials page

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const closeBtn = document.querySelector(".close");
+
+  document.querySelectorAll(".clickable-image").forEach(img => {
+    img.addEventListener("click", function () {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+    });
+  });
+
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+
+
